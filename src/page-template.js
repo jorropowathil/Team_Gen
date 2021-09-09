2719ba1eb4e73c691f2f74010788a8367840c89c
jorropowathil <85700575+jorropowathil@users.noreply.github.com>
2021-09-09 16:47:32 -0500

    finished engineer lib file

--- src/page-template.js
+++ src/page-template.js
@@ -1,3 +1,7 @@
+const engineArray = [];
+const manageArray = [];
+const internsArray = [];
+
 //create function that organizes information for the team and returns html for each type of team member inside a html template
 
 //will need individual functions to take in team member and return the appropiate info
